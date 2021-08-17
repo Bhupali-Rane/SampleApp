28aa00c6e1c3a9c7f352435f5cc13133eb5859d9
bhurane <bhurane@LIN25005437.corp.capgemini.com>
2021-08-17 13:27:21 +0530

    salesclass added

---
+++ sampleapp/src/main/java/sampleapp/salesclass.java
@@ -0,0 +1,15 @@
+package sampleapp;
+
+public class salesclass {
+	public void display() {
+		int[] arr= {23,45,56,78,6};
+		for(int num:arr)
+			System.out.println(num);
+	}
+
+	public void print() {
+		System.out.println("Print all array numbers :");
+		display();
+	}
+
+}
